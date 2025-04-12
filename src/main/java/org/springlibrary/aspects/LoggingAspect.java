ad901032b84689e17b9d5bd5eb07dc352d722c31
druidmentalized <dmitry.barmuta@gmail.com>
2025-04-12 22:46:19 +0200

    Added time logging for the LoggingAspect

--- src/main/java/org/springlibrary/aspects/LoggingAspect.java
+++ src/main/java/org/springlibrary/aspects/LoggingAspect.java
@@ -5,6 +5,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @Aspect
@@ -12,19 +14,22 @@ import java.util.Arrays;
 public class LoggingAspect {
 
     @Around("execution(* org.springlibrary.services..*(..))")
-    public Object logMethod(ProceedingJoinPoint joinPoint) {
+    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().toShortString();
         Object[] args = joinPoint.getArgs();
-        System.out.println("Calling method: " + methodName + " with args: " + Arrays.toString(args));
+
+        String currentTime = "[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")) + "] ";
+        System.out.println(currentTime + "Calling method: " + methodName + " with args: " + Arrays.toString(args));
+
         Object result;
         try {
             result = joinPoint.proceed();
-            System.out.println("Method succeeded with result: " + result);
+            System.out.println(currentTime + "Method succeeded with result: " + result);
         } catch (Throwable e) {
-            System.out.println("Method failed with exception: " + e.getMessage());
-            throw new RuntimeException(e);
+            System.out.println(currentTime + "Method failed with exception: " + e.getMessage());
+            throw e;
         }
+
         return result;
     }
-
 }
