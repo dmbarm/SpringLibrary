05aac8a3977ebf4e660bdb44808f725f3af5d263
druidmentalized <dmitry.barmuta@gmail.com>
2025-06-23 19:53:23 +0200

    Added locale resolver in order to successfully show mistakes in proper language

--- src/main/java/org/springlibrary/config/AppConfig.java
+++ src/main/java/org/springlibrary/config/AppConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -30,6 +32,13 @@ public class AppConfig implements WebMvcConfigurer {
         source.setDefaultEncoding(encoding);
 
         return source;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
     }
 
     @Bean
